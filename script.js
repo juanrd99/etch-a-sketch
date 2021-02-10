fc6795e6ee500180c458d42104aa6f8a42972082
Felipe Layos <jfelipelayos@gmail.com>
2021-02-10 12:10:03 -0500

    Improvement of code, added size picker and favicon

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 const container = document.getElementById("container");
 const clearBtn = document.getElementById("clearBtn");
 
-clearBtn.addEventListener("click", clearGrid);
-window.addEventListener("load", createGrid(32, 32));
-
-function createGrid(rows, cols) {
+const createGrid = (rows, cols) => {
+    clearGrid();
     container.style.setProperty("--grid-rows", rows);
     container.style.setProperty("--grid-cols", cols);
-    for (c = 0; c < rows * cols; c++) {
-        let cell = document.createElement("div");
-        container.appendChild(cell).className = "grid-item";
-        cell.addEventListener("mouseover", changeColor);
+    for (i = 0; i < rows * cols; i++) {
+        let grid = document.createElement("div");
+        container.appendChild(grid).className = "grid-item";
+        grid.addEventListener("mouseover", changeColor);
     }
-}
+};
 
-function changeColor(e) {
+const changeColor = (e) => {
     const randomR = Math.floor(Math.random() * 256);
     const randomG = Math.floor(Math.random() * 256);
     const randomB = Math.floor(Math.random() * 256);
     e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
-}
+};
 
-function clearGrid() {
+const clearGrid = () => {
     var gridItems = container.querySelectorAll("div");
     gridItems.forEach((item) => (item.style.backgroundColor = "#ffffff"));
-}
+};
+
+clearBtn.addEventListener("click", clearGrid);
+window.addEventListener("load", createGrid(32, 32));
